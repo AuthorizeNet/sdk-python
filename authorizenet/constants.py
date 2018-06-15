8d36834bf7a23b0f477662761c8099f3cd071fe4
Ashutosh Singh <ashtru@users.noreply.github.com>
2018-06-15 12:54:12 +0530

    Updated to 1.1.0

--- authorizenet/constants.py
+++ authorizenet/constants.py
@@ -16,7 +16,7 @@ class constants(object):
     PRODUCTION = 'https://api2.authorize.net/xml/v1/request.api'
 
     '''clientId'''
-    version = '1.0.16'
+    version = '1.1.0'
     clientId = 'sdk-python-' + version
 
     '''xml encoding'''
